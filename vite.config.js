81ca4bb3faceceaef64d784d5695503a7b86b4c9
mirrormouse <kagamidokei@gmail.com>
2025-06-23 23:06:18 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
-// https://vite.dev/config/
+// ★ リポジトリ名が公開 URL に入る場合は必ず base を設定
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/hakoiri-musume/",     // 例: https://<USER>.github.io/**hakoiri-musume**/
+  plugins: [react(), ghPages()],
+});
